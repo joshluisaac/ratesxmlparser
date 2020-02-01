216de0fed871d2ec86f45eb874773fd482bce4d8
Josh Uzo <nwankwo.joshua@gmail.com>
2020-02-02 09:34:33 +1100

    Initial commit

---
+++ infrastructure/xmlParser.js
@@ -0,0 +1,9 @@
+var rate = {
+    key: 'L1-var',
+    value: 4.87
+};
+var rates = [];
+var myString;
+rates.push({ key: "", value: 90 });
+rates.push({ key: "", value: 90 });
+rates.push({ key: "", value: 90 });
---
+++ infrastructure/xmlParser.ts
@@ -0,0 +1,20 @@
+interface Rate {
+    key: String;
+    value: Number;
+}
+
+const rate: Rate = {
+    key: 'L1-var',
+    value: 4.87
+}
+
+
+const rates: Rate[] = [];
+
+
+let myString:String;
+
+
+rates.push({key:"",value:90})
+rates.push({key:"",value:90})
+rates.push({key:"",value:90})
