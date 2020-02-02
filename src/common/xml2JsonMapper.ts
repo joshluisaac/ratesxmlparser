b9e2292ee4152bced28df63c40ad53927115cf12
Josh Uzo <nwankwo.joshua@gmail.com>
2020-02-03 00:37:08 +1100

    Updated comments

--- src/common/xml2JsonMapper.ts
+++ src/common/xml2JsonMapper.ts
@@ -5,11 +5,15 @@ export class Xml2JsonMapper {
 
     private static readonly settings = {explicitArray: false};
 
+    /**
+     * Returns a JSON string from an XML content.
+     * @param xmlContent
+     */
     public static parseXml(xmlContent: string): any {
         const parser = new xml2js.Parser(Xml2JsonMapper.settings);
         let json: any = {};
         parser.parseString(xmlContent, (error: any, parsedContent: any) => {
-            //consider calling an error handling function
+            //Josh: consider calling or dispatching an error handling function here.
             if (error)  throw error;
             json = parsedContent;
         });
