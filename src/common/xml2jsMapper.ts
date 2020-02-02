3c3687a1cb19e917ed85fd86fb3d4da09733bad0
Josh Uzo <nwankwo.joshua@gmail.com>
2020-02-02 19:42:05 +1100

    Code formatting updates

--- src/common/xml2jsMapper.ts
+++ src/common/xml2jsMapper.ts
@@ -1,15 +1,18 @@
 import xml2js = require("xml2js");
 
-const xml2jsSettings = { explicitArray : false };
+const xml2jsSettings = {explicitArray: false};
 
 export class Xml2jsMapper {
 
-    public static parseXml(xmlContent:string) : any {
+    public static parseXml(xmlContent: string): any {
         let parser = new xml2js.Parser(xml2jsSettings);
         let json: any = {};
         parser.parseString(xmlContent, function (error: any, parsedContent: any) {
-            if (error) { throw error; }
-            else { json = parsedContent; }
+            if (error) {
+                throw error;
+            } else {
+                json = parsedContent;
+            }
         });
         return json;
     }
--- src/rate/rateService.ts
+++ src/rate/rateService.ts
@@ -4,25 +4,12 @@ export class RateService {
 
     private rates = new Map<string, string>();
 
-    constructor(xml:string) {
+    constructor(xml: string) {
         this.setRates(xml);
     }
 
     private static parseRate(xml: string): string {
         return Xml2jsMapper.parseXml(xml).rates.rate;
-    }
-
-    private toMap(rate:any) : Map<string,string> {
-        let rates = new Map<string,string>();
-        rate.map(entry => {
-            rates.set(entry.$.key,entry.$.value);
-        });
-        return rates;
-    }
-
-    private setRates(xml: string): void {
-        let parsedRate = RateService.parseRate(xml);
-        this.rates = this.toMap(parsedRate);
     }
 
     public getProductRate(productType: string): String {
@@ -31,6 +18,19 @@ export class RateService {
 
     public getProductRates(): Map<string, string> {
         return this.rates;
+    }
+
+    private toMap(rate: any): Map<string, string> {
+        let rates = new Map<string, string>();
+        rate.map(entry => {
+            rates.set(entry.$.key, entry.$.value);
+        });
+        return rates;
+    }
+
+    private setRates(xml: string): void {
+        let parsedRate = RateService.parseRate(xml);
+        this.rates = this.toMap(parsedRate);
     }
 
 }
